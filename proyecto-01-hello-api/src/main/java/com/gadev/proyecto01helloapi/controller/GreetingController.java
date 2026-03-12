4ff5bbc885a59d2a2ea6e50a6b89e79db21c21ec
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-12 16:51:17 -0500

    feat: create calcular endpoint with path variables

--- proyecto-01-hello-api/src/main/java/com/gadev/proyecto01helloapi/controller/GreetingController.java
+++ proyecto-01-hello-api/src/main/java/com/gadev/proyecto01helloapi/controller/GreetingController.java
@@ -1,9 +1,6 @@
 package com.gadev.proyecto01helloapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,12 +10,12 @@ import java.time.format.DateTimeFormatter;
 public class GreetingController {
 
     @GetMapping("/saludo")
-    public String saludar(@RequestParam(defaultValue = "Mundo") String nombre){
+    public String saludar(@RequestParam(defaultValue = "Mundo") String nombre) {
         return "Hola " + nombre + "!";
     }
 
     @GetMapping("/fecha")
-    public String obtenerFecha(){
+    public String obtenerFecha() {
         // Obtenemos la fecha y hora actual del servidor
         LocalDateTime ahora = LocalDateTime.now();
         // Definimos el formato en el que se va mostrar la fecha y hora
@@ -29,18 +26,47 @@ public class GreetingController {
     }
 
     @GetMapping("/momento")
-    public String saludoPorMomento(){
+    public String saludoPorMomento() {
         // Obtenemos la hora actual (formato 24 horas, de 0 a 23)
         int hora = LocalDateTime.now().getHour();
 
         // Retornamos saludo dependiendo de la hora
-        if (hora >= 6 && hora < 12){
+        if (hora >= 6 && hora < 12) {
             return "Buenos dias!";
-        } else if (hora >= 12 && hora < 19 ){
+        } else if (hora >= 12 && hora < 19) {
             return "Buenas tardes!";
         } else {
             return "Buenas noches!";
         }
+    }
+
+    @GetMapping("/calcular/{n1}/{op}/{n2}")
+    public String calcular(
+            @PathVariable double n1,
+            @PathVariable String op,
+            @PathVariable double n2
+    ) {
+        double resultado;
+
+        switch (op){
+            case "sumar" -> resultado = n1 + n2;
+            case "restar" -> resultado = n1 - n2;
+            case "multiplicar" -> resultado = n1 * n2;
+            case "dividir" ->
+            {
+                if (n1 == 0 || n2 == 0){
+                    return "No se puede dividir por 0";
+                }
+                resultado = n1 / n2;
+            }
+            default -> {
+                return "Operación no valida. Usa: sumar, restar, multiplicar, dividir";
+            }
+
+        }
+
+        return n1 + " " + op + " " + n2 + " = " + resultado;
+
     }
 
 }
