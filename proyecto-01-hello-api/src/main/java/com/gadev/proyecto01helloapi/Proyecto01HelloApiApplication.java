168b5ce56a5a2b3bcd106518ba6d02308a59b501
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-12 15:52:43 -0500

    feat: agregar proyecto-01-hello-api

---
+++ proyecto-01-hello-api/src/main/java/com/gadev/proyecto01helloapi/Proyecto01HelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.gadev.proyecto01helloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto01HelloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto01HelloApiApplication.class, args);
+    }
+
+}
---
+++ proyecto-01-hello-api/src/test/java/com/gadev/proyecto01helloapi/Proyecto01HelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadev.proyecto01helloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto01HelloApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
