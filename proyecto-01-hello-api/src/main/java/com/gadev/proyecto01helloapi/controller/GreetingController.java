6c9cb885282a1a85ea0647b8ceb6bbefacb439cc
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-12 16:00:50 -0500

    feat: create fecha endpoint showing current date and time formatted

--- proyecto-01-hello-api/src/main/java/com/gadev/proyecto01helloapi/controller/GreetingController.java
+++ proyecto-01-hello-api/src/main/java/com/gadev/proyecto01helloapi/controller/GreetingController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @RequestMapping("/api")
 public class GreetingController {
@@ -12,6 +15,17 @@ public class GreetingController {
     @GetMapping("/saludo")
     public String saludar(@RequestParam(defaultValue = "Mundo") String nombre){
         return "Hola " + nombre + "!";
+    }
+
+    @GetMapping("/fecha")
+    public String obtenerFecha(){
+        // Obtenemos la fecha y hora actual del servidor
+        LocalDateTime ahora = LocalDateTime.now();
+        // Definimos el formato en el que se va mostrar la fecha y hora
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        // retornamos la fecha formato
+        return "Fecha y hora actual: " + ahora.format(formato);
     }
 
 }
