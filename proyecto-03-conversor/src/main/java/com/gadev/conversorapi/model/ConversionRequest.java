d4c0aa711e9fa362bfaa7b1af1d1988f8920ed60
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-12 17:37:17 -0500

    feat: add ConversionRequest and ConversionResponse models for conversion data

--- proyecto-03-conversor/src/main/java/com/gadev/conversorapi/model/ConversionRequest.java
+++ proyecto-03-conversor/src/main/java/com/gadev/conversorapi/model/ConversionRequest.java
@@ -1,4 +1,34 @@
 package com.gadev.conversorapi.model;
 
 public class ConversionRequest {
+
+    private double valor;
+    private String desde;
+    private String hacia;
+
+    public ConversionRequest(){}
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDesde() {
+        return desde;
+    }
+
+    public void setDesde(String desde) {
+        this.desde = desde;
+    }
+
+    public String getHacia() {
+        return hacia;
+    }
+
+    public void setHacia(String hacia) {
+        this.hacia = hacia;
+    }
 }
--- proyecto-03-conversor/src/main/java/com/gadev/conversorapi/model/ConversionResponse.java
+++ proyecto-03-conversor/src/main/java/com/gadev/conversorapi/model/ConversionResponse.java
@@ -1,4 +1,58 @@
 package com.gadev.conversorapi.model;
 
 public class ConversionResponse {
+
+    private double valorOriginal;
+    private String unidadOriginal;
+    private double valorConvertido;
+    private String unidadDestino;
+    private String formula;
+
+    public ConversionResponse(double valorOriginal, String unidadOriginal, double valorConvertido, String unidadDestino, String formula) {
+        this.valorOriginal = valorOriginal;
+        this.unidadOriginal = unidadOriginal;
+        this.valorConvertido = valorConvertido;
+        this.unidadDestino = unidadDestino;
+        this.formula = formula;
+    }
+
+    public double getValorOriginal() {
+        return valorOriginal;
+    }
+
+    public void setValorOriginal(double valorOriginal) {
+        this.valorOriginal = valorOriginal;
+    }
+
+    public String getUnidadOriginal() {
+        return unidadOriginal;
+    }
+
+    public void setUnidadOriginal(String unidadOriginal) {
+        this.unidadOriginal = unidadOriginal;
+    }
+
+    public double getValorConvertido() {
+        return valorConvertido;
+    }
+
+    public void setValorConvertido(double valorConvertido) {
+        this.valorConvertido = valorConvertido;
+    }
+
+    public String getUnidadDestino() {
+        return unidadDestino;
+    }
+
+    public void setUnidadDestino(String unidadDestino) {
+        this.unidadDestino = unidadDestino;
+    }
+
+    public String getFormula() {
+        return formula;
+    }
+
+    public void setFormula(String formula) {
+        this.formula = formula;
+    }
 }
