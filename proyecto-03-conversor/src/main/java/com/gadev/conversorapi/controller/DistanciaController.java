f5c396898ff04e6ae668bfbad88cdbc6a1314cff
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-12 19:59:35 -0500

    feat: add controllers for distance, weight, and temperature conversion with request/response handling

--- proyecto-03-conversor/src/main/java/com/gadev/conversorapi/controller/DistanciaController.java
+++ proyecto-03-conversor/src/main/java/com/gadev/conversorapi/controller/DistanciaController.java
@@ -1,4 +1,26 @@
 package com.gadev.conversorapi.controller;
 
+import com.gadev.conversorapi.model.ConversionRequest;
+import com.gadev.conversorapi.model.ConversionResponse;
+import com.gadev.conversorapi.service.DistanciaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/convertir/distancia")
 public class DistanciaController {
+
+    private final DistanciaService distanciaService;
+
+    public DistanciaController(DistanciaService distanciaService) {
+        this.distanciaService = distanciaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ConversionResponse> convertir(@RequestBody ConversionRequest request){
+        return ResponseEntity.ok(distanciaService.convertir(request));
+    }
 }
--- proyecto-03-conversor/src/main/java/com/gadev/conversorapi/controller/TemperaturaController.java
+++ proyecto-03-conversor/src/main/java/com/gadev/conversorapi/controller/TemperaturaController.java
@@ -1,4 +1,26 @@
 package com.gadev.conversorapi.controller;
 
+import com.gadev.conversorapi.model.ConversionRequest;
+import com.gadev.conversorapi.model.ConversionResponse;
+import com.gadev.conversorapi.service.TemperaturaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/convertir/temperatura")
 public class TemperaturaController {
+
+    private final TemperaturaService temperaturaService;
+
+    public TemperaturaController(TemperaturaService temperaturaService) {
+        this.temperaturaService = temperaturaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ConversionResponse> convertir(@RequestBody ConversionRequest request){
+        return ResponseEntity.ok(temperaturaService.convertir(request));
+    }
 }
